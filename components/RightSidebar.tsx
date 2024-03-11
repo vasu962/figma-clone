b7ec9e286ad6b1bb1bff88a4b50afed427c7914c
Vasu <vasukumar962@gmail.com>
2024-03-12 00:37:30 +0530

    added export file feature

--- components/RightSidebar.tsx
+++ components/RightSidebar.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import Dimensions from './settings/Dimensions';
 import Text from './settings/Text';
 import Color from './settings/Color';
@@ -14,6 +14,9 @@ const RightSidebar = ({
   isEditingRef,
   syncShapeInStorage,
 }: RightSidebarProps) => {
+    const colorInputRef = useRef(null);
+    const strokeInputRef = useRef(null);
+
     const handleInputChange = (property: string, value: string) => {
       if(!isEditingRef.current) isEditingRef.current = true;
 
@@ -57,8 +60,20 @@ const RightSidebar = ({
       fontWeight={elementAttributes.fontWeight}
       handleInputChange={handleInputChange}
       />
-      <Color />
-      <Color />
+      <Color 
+      inputRef={colorInputRef}
+      attribute={elementAttributes.fill}
+      attributeType='fill'
+      placeholder='color'
+      handleInputChange={handleInputChange}
+      />
+      <Color 
+      inputRef={strokeInputRef}
+      attribute={elementAttributes.stroke}
+      attributeType='stroke'
+      placeholder='stroke'
+      handleInputChange={handleInputChange}
+      />
       <Export />
     </section>
   );
